15609cbcef38a97abfcb881219d9f64bc5d96a97
duy-kt-gnk <duy-kt@genkisystem.com>
2023-11-17 13:33:38 +0700

    Incude image editor css

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,2 +1,9 @@
-declare module "*.css";
-declare module "*.scss";
+// For SCSS modules
+declare module '*.scss' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  
+  // For regular CSS
+  declare module '*.css';
+  
--- src/image-editor/ImageEditor.ts
+++ src/image-editor/ImageEditor.ts
@@ -1,33 +1,18 @@
-import ImageEditor from "tui-image-editor";
+import ImageEditor from 'tui-image-editor';
+
+import './tui-image-editor/tui-image-editor.css';
+import './tui-image-editor/tui-color-picker.css';
 
 export class ImageEditorWrapper {
   private imageEditor: ImageEditor | null;
-  private readonly tuiImageEditorStylesheet =
-    "https://uicdn.toast.com/tui-image-editor/latest/tui-image-editor.min.css";
-  private readonly tuiColorPickerStylesheet =
-    "https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.css";
-
+  
   constructor(editorId: string, initialImage: string) {
-    this.loadStylesheets();
     this.imageEditor = this.createImageEditor(editorId, initialImage);
     this.removeUnnecessaryElements();
   }
 
   public getImageDataUrl(): string | undefined {
     return this.imageEditor?.toDataURL();
-  }
-
-  private loadStylesheets() {
-    console.log("[Image editor] loadStylesheets started");
-    this.loadStylesheet(this.tuiImageEditorStylesheet);
-    this.loadStylesheet(this.tuiColorPickerStylesheet);
-  }
-
-  private loadStylesheet(href: string) {
-    const linkElement = document.createElement("link");
-    linkElement.rel = "stylesheet";
-    linkElement.href = href;
-    document.head.appendChild(linkElement);
   }
 
   private createImageEditor(
@@ -49,7 +34,6 @@ export class ImageEditorWrapper {
           menuBarPosition: "bottom",
           usageStatistics: false,
         },
-        cssMaxWidth: 500,
         cssMaxHeight: 500,
       };
 
