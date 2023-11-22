ab66350e8b7ab13122e9a586634c9c073c6f86d9
huanlam <huan-lt@genkisystem.com>
2023-11-22 15:07:23 +0700

    fix code after merge

--- src/index.ts
+++ src/index.ts
@@ -156,9 +156,9 @@ class MagicPoint extends Base {
                 <div class="${css['second-col']}">
                     <div class="${css['input-field']} ">
                         <label class="${css['title-label']}" for="title">Task title: </label>
-                        <input type="text" id="title">
+                        <input type="text" id="task-title">
                     </div>
-                    <div class="${css.action}>
+                    <div class="${css['action']}" id="submit-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                         </svg>
@@ -197,7 +197,7 @@ class MagicPoint extends Base {
         })
         console.log(quill)
 
-        const imageEditorWrapper = new ImageEditorWrapper(`#${css['canvas-holder']}`, canvas.toDataURL());
+        imageEditorWrapper = new ImageEditorWrapper(`#${css['canvas-holder']}`, canvas.toDataURL());
         console.log(imageEditorWrapper);
     }
 
